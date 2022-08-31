e9b1da9017c0849b92a9bbc05a93b53c968eff3b
wisberry <wisberry2022@gmail.com>
2022-08-31 17:45:47 +0900

    08/31 upload

--- js/main.js
+++ js/main.js
@@ -1,56 +1,100 @@
 $(function () {
-  /////////////////////////////////////
-  $('.top_close_btn').on('click', function () {
-    $('.TopBanner').addClass('on');
-    $('.Section01').addClass('on');
-  });
+    /////////////////////////////////////
+    $('.top_close_btn').on('click', function () {
+        $('.TopBanner').addClass('on');
+        $('.Section01').addClass('on');
+    });
 
-  $('.main_slider').slick({
-    arrows: false,
-  });
+    $('.main_slider').on('init reInit afterChange', function (e, ms, c) {
+        var current = $('.main_slider .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+        $('.slide_dots li').eq(c).addClass('on').siblings().removeClass('on');
+        $('.Section01 .num').html('0' + ((c ? c : 0) + 1) + '<span> / 0' + ms.slideCount + '</span>');
+    });
 
-  $('.left_slider').slick({
-    arrows: false,
-    asNavFor: '.right_slider',
-    fade: true,
-  });
+    $('.Section01 .slide_dots li').on('click', function () {
+        var idx = $(this).index();
+        console.log(idx)
+        $('.main_slider').slick('slickGoTo', idx)
+    })
 
-  $('.right_slider').slick({
-    arrows: false,
-    asNavFor: '.left_slider',
-    slidesToShow: 5,
-  });
+    $('.main_slider').slick({
+        arrows: false,
+    });
 
-  $('.center_slide').on('init reInit afterChange', function (e, s, c) {
-    $('.num').html('0' + (c ? c + 1 : 1) + '<span> / 0' + s.slideCount + '</span>');
-    var current = $('.center_slide .slick-current');
-    current.addClass('on').siblings().removeClass('on');
-    $('.content_box>div').eq(c).addClass('on').siblings().removeClass('on');
-  })
-
-  $('.center_slide').slick({
-    arrows: false,
-    centerMode: true,
-    centerPadding: '300px',
-  });
+    $('.Section01 .slide_handler button:first-child').on('click', function () {
+        $('.main_slider').slick('slickPrev')
+    })
+    $('.Section01 .slide_handler button:last-child').on('click', function () {
+        $('.main_slider').slick('slickNext')
+    })
 
 
 
-  $('.slide_handler i:first-child').on('click', function () {
-    $('.left_slider').slick('slickPrev');
-  });
+    $('.left_slider').slick({
+        arrows: false,
+        fade: true,
+        asNavFor: '.right_slider'
+    });
+    $('.right_slider').slick({
+        arrows: false,
+        asNavFor: '.left_slider',
+        slidesToShow: 5,
+    });
 
-  $('.slide_handler i:last-child').on('click', function () {
-    $('.left_slider').slick('slickNext');
-  });
-
-  $(window).on('scroll', function () {
-    var sct = $(window).scrollTop();
-    sct > 0
-      ? $('.Header').addClass('on')
-      : $('.Header').removeClass('on')
-  })
+    $('.center_slider').on('init reInit afterChange', function (e, s, c) {
+        console.log(s.slideCount);
+        var current = $('.center_slider .slick-current');
+        current.addClass('on').siblings().removeClass('on');
+        console.log(current);
+        $('.Section04 .num').html((c ? c : 0) + 1 + '<span> / 0' + s.slideCount + '</span>');
+        $('.content_box>div').eq(c).addClass('on').siblings().removeClass('on');
+    });
 
 
-  /////////////////////////////////////
+    $('.center_slider').slick({
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        centerPadding: '300px',
+    });
+
+
+    $('.Section04 .slide_handler i:first-child').on('click', function () {
+        $('.center_slider').slick('slickPrev')
+    });
+    $('.Section04 .slide_handler i:last-child').on('click', function () {
+        $('.center_slider').slick('slickNext')
+    })
+
+
+
+    $('.slide_handler i:first-child').on('click', function () {
+        $('.left_slider').slick('slickPrev')
+    });
+    $('.slide_handler i:last-child').on('click', function () {
+        $('.left_slider').slick('slickNext')
+    });
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        sct > 0
+            ? $('.Header').addClass('on')
+            : $('.Header').removeClass('on')
+    });
+
+    // pop up cookie
+
+    console.log($.cookie('todayPopUp'));
+    var TCK = $.cookie('todayPopUp');
+    if (!TCK) {
+        $('.today_pop').show();
+    }
+
+    $('#Today').on('change', function () {
+        $.cookie('todayPopUp', '팝업쿠키', { expires: 0.1 });
+        $('.today_pop').hide();
+        //출처: https://offbyone.tistory.com/176 [쉬고 싶은 개발자:티스토리]
+    })
+    /////////////////////////////////////
 })
